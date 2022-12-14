b50a3ea028e0205050ed1e231a3909d978cfcb23
Tonnie91 <gathogoanthony2@gmail.com>
2022-12-14 22:37:11 +0300

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Buy and sell quality study notes and resources</title>
 
     
     <link rel="icon" href="{{asset('theme/img/favicon.png')}}" type="image/png" />
