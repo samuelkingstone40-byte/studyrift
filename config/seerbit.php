d4879966a5d96280eea3c963a7a8b1ae9d67a4a3
TonnieGathogo <gathogoanthony2@gmail.com>
2023-11-13 12:59:20 +0300

    update routes

--- config/seerbit.php
+++ config/seerbit.php
@@ -1,7 +1,7 @@
 <?php 
 
 return [
-    'environment'=>env('SEERBIT_ENVIRONMENT', \Seerbit\Environment::LIVE),
+    'environment'=>env('SEERBIT_ENVIRONMENT'),
     'public_key' => env('SEERBIT_PUBLIC_KEY'),
     'secret_key' => env('SEERBIT_SECRET_KEY'),
    
