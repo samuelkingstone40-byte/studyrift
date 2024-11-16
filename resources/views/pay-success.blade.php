e022dcea6a86749b3bc7893f725bcef6dce11657
TonnieGathogo <gathogoanthony2@gmail.com>
2024-11-16 14:09:31 +0300

    fix payment

--- resources/views/pay-success.blade.php
+++ resources/views/pay-success.blade.php
@@ -2,7 +2,7 @@
 <title>Payment-Studymerit</title>
 @section('content')
 <section class="section_gap">
-    <div class="container">
+
         <div class="text-center bg-white p-4">
             <h1 class="display-4 text-success">Success! <i class="fa fa-smile-o" aria-hidden="true"></i></h1>
             <p class="lead my-4"><strong>Your payment has been received</strong> Proceed to download your documents.</p>
@@ -44,6 +44,6 @@
   Having trouble? <a href="{{route('contact')}}">Contact us</a>
 </p>
 </div>
-    </div>
+    
 </section>
 @endsection
