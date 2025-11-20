40f70d2f28bb53ffebd35b2008e108e9e5889f4d
samuelkingstone40-byte <your-email@example.com>
2025-11-20 12:38:23 +0300

    Set absolute favicon URLs (cache-bust) for StudyRift

--- resources/views/partials/favicon.blade.php
+++ resources/views/partials/favicon.blade.php
@@ -1,7 +1,6 @@
-<link rel="icon" href="{{ asset('favicon.ico') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
-<link rel="manifest" href="{{ asset('site.webmanifest') }}">
-<meta name="msapplication-TileColor" content="#2b5797">
+<link rel="icon" href="https://studyrift.com/favicon.ico?v=1763631420">
+<link rel="shortcut icon" href="https://studyrift.com/favicon.ico?v=1763631420">
+<link rel="icon" type="image/png" sizes="32x32" href="https://studyrift.com/favicon-32x32.png?v=1763631420">
+<link rel="apple-touch-icon" sizes="180x180" href="https://studyrift.com/apple-touch-icon.png?v=1763631420">
+<link rel="manifest" href="https://studyrift.com/site.webmanifest?v=1763631420">
 <meta name="theme-color" content="#ffffff">
