cc74cf79c28342ecac4d69d61b20c2fd193956b3
samuelkingstone40-byte <your-email@example.com>
2025-11-20 12:47:20 +0300

    Temporary test: add favicon-test and point partial at it (cache-bust)

--- resources/views/partials/favicon.blade.php
+++ resources/views/partials/favicon.blade.php
@@ -1,6 +1,4 @@
-<link rel="icon" href="https://studyrift.com/favicon.ico?v=1763631420">
-<link rel="shortcut icon" href="https://studyrift.com/favicon.ico?v=1763631420">
-<link rel="icon" type="image/png" sizes="32x32" href="https://studyrift.com/favicon-32x32.png?v=1763631420">
-<link rel="apple-touch-icon" sizes="180x180" href="https://studyrift.com/apple-touch-icon.png?v=1763631420">
-<link rel="manifest" href="https://studyrift.com/site.webmanifest?v=1763631420">
+<link rel="icon" href="https://studyrift.com/favicon-test.ico?v='$TS'">
+<link rel="shortcut icon" href="https://studyrift.com/favicon-test.ico?v='$TS'">
+<link rel="manifest" href="https://studyrift.com/site.webmanifest?v='$TS'">
 <meta name="theme-color" content="#ffffff">
